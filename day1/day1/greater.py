d7d2d4185e43e7e3aae26fb256d30fb96a19c101
Kaviraj <137800764+Kaviraj032003@users.noreply.github.com>
2023-07-11 13:56:48 +0530

    Delete day1/day1 directory

--- day1/day1/greater.py
+++
@@ -1,9 +0,0 @@
-x=int(input("Enter  number x:"))
-y=int(input("Enter number y: "))
-
-if x>y:
-    print("x is greater than y")
-elif x<y:
-    print("y is greater than x")
-else:
-    print("x and y are equal")
--- day1/day1/pos-neg.py
+++
@@ -1,9 +0,0 @@
-N=int(input("Enter any number:"))
-if N<0:
-    print("Negative number")
-elif N==0:
-    print("Zero")
-elif N>0:
-    print("positive number")
-else:
-    print("invalid input")
